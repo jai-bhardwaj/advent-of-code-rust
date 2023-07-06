eac48300b41ca416b2f4713c90c25854dd94760b
Sujal Sharma <0987sujals@gmail.com>
2023-07-06 16:10:33 +0530

    Completed the part-2 of the day-01.

--- 2022/day-01/src/lib.rs
+++ 2022/day-01/src/lib.rs
@@ -1,22 +1,27 @@
-
 pub fn process_part_1(input: &str) -> i32 {
-    input.split("\n\n")
-        .map(|x| {
-            x.lines()
-                .map(|y| y.parse::<i32>().unwrap())
-                .sum::<i32>()
-        })
+    input
+        .split("\n\n")
+        .map(|x| x.lines().map(|y| y.parse::<i32>().unwrap()).sum::<i32>())
         .max()
         .unwrap()
+}
+
+pub fn process_part_2(input: &str) -> i32 {
+    let mut res = input
+        .split("\n\n")
+        .map(|x| x.lines().map(|y| y.parse::<i32>().unwrap()).sum::<i32>())
+        .collect::<Vec<i32>>();
+    res.sort_by(|a,b| b.cmp(a));
+    let res: i32 = res.iter().take(3).sum();
+    res
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let input = "1000
+    const INPUT: &str = "1000
 2000
 3000
 
@@ -30,7 +35,16 @@ mod tests {
 9000
 
 10000";
-        let result = process_part_1(input);
+
+    #[test]
+    fn it_works() {
+        let result = process_part_1(INPUT);
         assert_eq!(result, 24000);
+    }
+
+    #[test]
+    fn process_part_2_works() {
+        let result = process_part_2(INPUT);
+        assert_eq!(result, 45000);
     }
 }
