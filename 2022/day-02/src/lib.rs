39831b999a67c4b75dfd4e8a6582eaa2bcbb6563
Sujal Sharma <0987sujals@gmail.com>
2023-07-11 14:07:01 +0530

    Completed the day-02 of aoc in rust

--- 2022/day-02/src/lib.rs
+++ 2022/day-02/src/lib.rs
@@ -6,65 +6,90 @@ enum Move {
 
 pub fn process_part_1(input: &str) -> i32 {
     input
-        .split("\n\n")
-        .map(|x| {
-            x.lines()
-            .map(|y| {
-                let opponent_option = y.chars().nth(0).unwrap();
-                let my_option = y.chars().nth(2).unwrap();
+        .split("\n")
+        .map(|moves| {
+            let (op_move, my_move) = moves.split_once(' ').unwrap();
+            println!("{} vs {}", op_move, my_move);
 
-                let mut score = 0;
+            let my_move  = match my_move {
+                "X" => Move::Rock,
+                "Y" => Move::Paper,
+                "Z" => Move::Scissors,
+                _ => unreachable!(),
+            };
+            let op_move = match op_move {
+                "A" => Move::Rock,
+                "B" => Move::Paper,
+                "C" => Move::Scissors,
+                _ => unreachable!(),
+            };
 
-                let op_move: Move = match opponent_option {
-                    'A' => Move::Rock,
-                    'B' => Move::Paper,
-                    'C' => Move::Scissors,
-                    _ => unreachable!()
-                };
+            let mut score = match my_move {
+                Move::Rock => 1,
+                Move::Paper => 2,
+                Move::Scissors => 3,
+            };
 
-                let my_move = match my_option {
-                    'X' => Move::Rock,
-                    'Y' => Move::Paper,
-                    'Z' => Move::Scissors,
-                    _ => unreachable!(),
-                };
+            score += match (op_move, my_move) {
+                (Move::Rock, Move::Rock) => 3,
+                (Move::Rock, Move::Paper) => 6,
+                (Move::Rock, Move::Scissors) => 0,
+                (Move::Paper, Move::Rock) => 0,
+                (Move::Paper, Move::Paper) => 3,
+                (Move::Paper, Move::Scissors) => 6,
+                (Move::Scissors, Move::Rock) => 6,
+                (Move::Scissors, Move::Paper) => 0,
+                (Move::Scissors, Move::Scissors) => 3,
+            };
 
-                score += &my_move as i32;
-
-                match &my_move {
-                    Move::Rock => {
-                        if op_move as i32 == Move::Scissors as i32 {
-                            score += 6;
-                        }
-                    },
-                    Move::Paper => {
-                        if op_move as i32 == Move::Rock as i32 {
-                            score += 6;
-                        }
-                    },
-                    Move::Scissors => {
-                        if op_move as i32 == Move::Paper as i32 {
-                            score += 6;
-                        }
-                    },
-                }
-                score
-                
-            })
-            .sum::<i32>()
-        })
-        .max()
-        .unwrap()
+            score
+        }).sum::<i32>()
 }
 
 pub fn process_part_2(input: &str) -> i32 {
-    let mut res = input
-        .split("\n\n")
-        .map(|x| x.lines().map(|y| y.parse::<i32>().unwrap()).sum::<i32>())
-        .collect::<Vec<i32>>();
-    res.sort_by(|a,b| b.cmp(a));
-    let res: i32 = res.iter().take(3).sum();
-    res
+     input
+        .split("\n")
+        .map(|moves| {
+            let (op_move, my_move) = moves.split_once(' ').unwrap();
+            // println!("{} vs {}", op_move, my_move);
+
+            // let my_move_expected = my_move.clone();
+            // let my_move  = match my_move {
+            //     "X" => Move::Rock,
+            //     "Y" => Move::Paper,
+            //     "Z" => Move::Scissors,
+            //     _ => unreachable!(),
+            // };
+            let op_move = match op_move {
+                "A" => Move::Rock,
+                "B" => Move::Paper,
+                "C" => Move::Scissors,
+                _ => unreachable!(),
+            };
+
+            let score = match op_move {
+                Move::Rock => match my_move {
+                        "X" => 0+3,
+                        "Y" => 3+1,
+                        "Z" => 6+2,
+                        _ => unreachable!(),
+                    },
+                Move::Paper => match my_move {
+                    "X" => 0+1,
+                    "Y" => 3+2,
+                    "Z" => 6+3,
+                    _ => unreachable!(),
+                },
+                Move::Scissors => match my_move {
+                    "X" => 0+2,
+                    "Y" => 3+3,
+                    "Z" => 6+1,
+                    _ => unreachable!(),
+                },
+            };
+
+            score
+        }).sum::<i32>()
 
 }
 
@@ -72,9 +97,13 @@ pub fn process_part_2(input: &str) -> i32 {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "A Y
-B X
-C Z";
+    const INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+";
 
     #[test]
     fn it_works() {
@@ -86,6 +115,6 @@ C Z";
     #[test]
     fn process_part_2_works() {
         let result = process_part_2(INPUT);
-        assert_eq!(result, 45000);
+        assert_eq!(result, 12);
     }
 }
